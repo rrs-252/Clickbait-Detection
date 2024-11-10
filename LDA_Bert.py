6b4f1ca817f7ef85ed3031d7721a77c12c9b35f9
Rishabhraj Srivastava <rishabhraj.srivastava2021@vitstudent.ac.in>
2024-11-10 20:52:14 +0530

    Update LDA_Bert.py

--- LDA_Bert.py
+++ LDA_Bert.py
@@ -47,8 +47,8 @@ def load_data_in_chunks(file_path, label, chunk_size=1000):
     return texts, labels
     
 # Paths to the datasets
-CLICKBAIT_PATH = "articlesClickbait.txt"
-NOT_CLICKBAIT_PATH = "articlesNotClickbait.txt"
+CLICKBAIT_PATH = "train_data/train_Clickbait.txt"
+NOT_CLICKBAIT_PATH = "train_data/train_NotClickbait.txt"
 
 # Load data with chunking
 texts = []
